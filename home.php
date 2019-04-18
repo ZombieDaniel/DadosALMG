08da01f1a33ad48374989b0f8035be22411b9c74
Daniel Barata <dcbarata@unifei.edu.br>
2019-04-18 11:35:30 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,31 @@
+<?php
+
+include 'index.php';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dados de Deputados</title>
+    <link rel="stylesheet" type="text/css" href = "estilo/estilo.css">
+</head>
+<body>
+    <div class="container">
+            <div id="conteudo">
+            <h2>Projeto API Dados Abertos da ALMG.</h2>
+            <br><br>
+            <h3>O que é o Sistema de Dados Abertos da ALMG?</h3>
+            <p>O Sistema de Webservices da ALMG, cuja sigla é ws, é composto por um conjunto de recursos web cujos 
+            acessos tentam, sempre que possível, atender aos ideais e ao conjunto de melhores práticas REST.
+            Outra forma de definir o ws seria como uma API REST. Possui uma API que retorna diversos tipos de POX 
+            através do HTTP.<br></p>
+
+            <h3>Por que utiliza-lo?</h3>
+            <p>Com esse sistema é possível acessar dados públicos sobre deputados, como: qual seu email, qual legislatura 
+                ele participou e várias outras informações bacanas.
+                Além de tudo isso, podemos saber como o dinheiro do nosso estado está sendo gasto.</p>
+
+
+</body>
+</html>
